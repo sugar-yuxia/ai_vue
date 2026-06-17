3d54a279cc124f02d7c23e0bf72aab0e50f51c7d
sugar-yuxia <3011601485@qq.com>
2026-06-17 17:28:24 +0800

    完善知识记录列表部分

--- ai-vue/src/api/admin.js
+++ ai-vue/src/api/admin.js
@@ -47,3 +47,7 @@ export function changeArticleStatus(id,data){
 export function deleteArticle(id){
     return service.delete(`/knowledge/article/${id}`)
 }
+
+export function getConsulationPage(params){
+    return service.get('/psychological-chat/sessions',{params})
+}
--- ai-vue/src/views/consulations.vue
+++ ai-vue/src/views/consulations.vue
@@ -1,5 +1,66 @@
 <template>
     <div>
-        <h1>咨询记录</h1>
+        <PageHead title="咨询记录" />
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column label="会话ID" width="100">
+                <template #default="scope">
+                    <el-avatar>{{ scope.row.userNickname }}</el-avatar>
+                </template>
+            </el-table-column>
+            <el-table-column label="情绪日志">
+                <template #default="scope">
+                    <div class="session-title">{{ scope.row.sessionTitle }}</div>
+                    <div class="session-preview">{{ scope.row.lastMessageContent }}</div>
+                </template>
+            </el-table-column>
+            <el-table-column prop="messageCount" label="消息数" width="100" />
+            <el-table-column prop="lastMessageTime" label="时间" width="100" />
+            <el-table-column label="操作" width="100">
+                <template #default="scope">
+                    <el-button type="primary" text @click="viewSessionDetail">详情</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination
+            style="margin-top: 25px;"
+            :page-size="pagination.size"
+            :total="pagination.total"
+            layout="prev, pager, next"
+            @change="handleChange"
+        />
     </div>
 </template>
+
+<script setup>
+import { ref, onMounted, reactive } from 'vue'
+import PageHead from '@/components/PageHead.vue'
+import { getConsulationPage } from '@/api/admin'
+
+const tableData = ref([])
+
+const pagination = reactive({
+    currentPage:1,
+    size:10,
+    total:0
+})
+
+const viewSessionDetail = () => {
+}
+
+const handleChange = (page) => {
+    pagination.currentPage = page
+    handleSearch()
+}
+
+const handleSearch = () => {
+    getConsulationPage(pagination).then(res => {
+        const { records,total }  = res
+        tableData.value = records
+        pagination.total = total
+    })
+}
+
+onMounted(() => {
+    handleSearch()
+})
+</script>
